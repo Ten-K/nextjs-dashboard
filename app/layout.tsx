22e3719b15e538e2b2bbb5bdbb2a78748c24007d
Ten-K <742798240@qq.com>
2024-07-18 22:19:35 +0800

    chapter end

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,23 @@
-import '@/app/ui/global.css';
+import "@/app/ui/global.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: {
+		template: "%s | Acme Dashboard",
+		default: "Acme Dashboard"
+	},
+	description: "The official Next.js Learn Dashboard built with App Router.",
+	metadataBase: new URL("https://next-learn-dashboard.vercel.sh")
+};
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
